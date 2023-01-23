2ecc9682a3beb066746297894982fde6c1811564
subashree3456 <subashree3003@gmail.com>
2023-01-23 12:57:43 +0530

    OK

---
+++ src/Components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () =>{
+    return(
+        <>
+        <div className="card">
+<img src="./images/book.jpg" alt="" />
+<div className="bottom">
+    <h3 className="title">React JS</h3>
+    <p className="amount"> &#8377;3290</p>
+</div>
+        </div>
+        </>
+    )
+}
+
+
+export default Card;
