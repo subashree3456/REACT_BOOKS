f62f88bacedc24f95b6f3ae645e38d7bd89d8fbf
subashree3456 <subashree3003@gmail.com>
2023-01-25 12:18:42 +0530

    NEW COMMIT

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React , {useState} from "react";
+import Modal from "./Modal";
 
-const Card = () =>{
-    return(
+const Card = ({ book }) => {
+
+  const [show,setShow]=useState(false);
+  const [bookItem,setItem]=useState();
+
+  console.log(book);
+  return (
+    <>
+      {book.map((item) => {
+
+let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;   // path in array object
+let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;      
+if(thumbnail!=undefined && amount!=undefined)
+{
+    return (
         <>
-        <div className="card">
-<img src="./images/book.jpg" alt="" />
-<div className="bottom">
-    <h3 className="title">React JS</h3>
-    <p className="amount"> &#8377;3290</p>
-</div>
+      <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
+      <img src={thumbnail} alt="" /> 
+        <div className="bottom">
+          <h3 className="title">{item.volumeInfo.title}</h3>
+          <p className="amount">&#8377;{amount}</p>
         </div>
-        </>
-    )
+      </div>
+      <Modal show={show} item={bookItem}  onClose={()=>setShow(false)}/>
+      </>
+    );  
+}
+      })
+      }
+    </>
+  )
 }
 
-
 export default Card;
+
+
+// old coding
+
+//<img src="./images/book.jpg" alt="" />
+
+
+
+// let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;   // path in array object
+//         return (
+//             <>
+//           <div className="card">
+//           <img src={thumbnail} alt="" /> 
+//             <div className="bottom">
+//               <h3 className="title">React JS</h3>
+//               <p className="amount"> &#8377;3290</p>
+//             </div>
+//           </div>
+//           </>
+//         );
+//       })}
+//     </>
+//   );
+// };
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React ,{useState} from "react";
 import Card from "./Card";
-
+import axios from "axios";
 const Main =() =>{
+const [ search , setSearch]=useState("");
+const [bookData , setData]=useState([]);
+const searchBook =(evt) =>{
+if(evt)
+{
+
+    axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+    // .then(res=>console.log(res))
+    // .then(res=>console.log(res.data))
+    // .then(res=>console.log(res.data.items))
+    .then(res=>setData(res.data.items))
+    .catch(err=>console.log(err))
+    // console.log("Hello");
+}
+}
+// MY API KEY  :    AIzaSyCaCyfNtODP25zF0869iNhiovEMauSe2aE
     return(
         <>
         <div className="header">
@@ -11,14 +27,17 @@ const Main =() =>{
             <div className="row2">
                 <h2>Find your Book</h2>
                 <div className="search">
-            <input type="text" placeholder="Enter your Book Name" />
+            <input type="text" placeholder="Enter your Book Name" value={search} onChange={e=>setSearch(e.target.value)}
+            onClick={searchBook}/>
             <button><i className="fas fa-search"></i></button>
                 </div>
                 <img src="./images/bg2.png" alt="" />
             </div>
         </div>
         <div className="container">
-<Card />
+      {
+          <Card book={bookData}/>
+      }
         </div>
         </>
     )
